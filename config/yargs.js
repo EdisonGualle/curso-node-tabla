b0e8c37251c8571f2b24f1dd601e962733c38247
EdisonGualle <ed.gualle@gmail.com>
2023-09-24 11:00:23 -0500

    Mejora de yargs

--- config/yargs.js
+++ config/yargs.js
@@ -6,17 +6,17 @@ const argv = require("yargs")
     demandOption: true,
     describe: 'Es la base de la tabla de multiplicar'
   })
-  .option("l", {
-    alias: "listar",
-    type: "boolean",
-    default: false,
-    describe : 'Muestra la tabla en consola'
-  })
   .option("h",{
     alias: "hasta",
     type:"number",
     default:10,
     describe: "Hasta que numero generar la tabla de multiplicar"
+  })
+  .option("l", {
+    alias: "listar",
+    type: "boolean",
+    default: false,
+    describe : 'Muestra la tabla en consola'
   })
   .check((argv, options) => {
     if (isNaN(argv.b)) {
